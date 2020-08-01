01a1c0c2e588771585eba38d54c7e763badfd2c2
povsister <pov@mahou-shoujo.moe>
2020-08-01 20:53:19 +0800

    Fix inconsistent log of Raven

--- player/Raven.go
+++ player/Raven.go
@@ -17,12 +17,14 @@ func (r *TheRaven) RoundAttack(round uint16) {
 		return
 	}
 	if round%3 == 0 {
-		log.Print("%s 发动技能 别墅小岛! 造成 7 x %d 点伤害", r.Name, r.finalDamage(r.Rival.Attributes().trueDamage(16)))
-		r.Rival.DirectTakeDamage(round, r.finalDamage(r.Rival.Attributes().trueDamage(16)), 7, Unique)
+		trueDam := r.finalDamage(r.Rival.Attributes().trueDamage(16))
+		log.Print("%s 发动技能 别墅小岛! 造成 7 x %d 点伤害", r.Name, trueDam)
+		r.Rival.DirectTakeDamage(round, trueDam, 7, Unique)
 		return
 	}
-	log.Print("%s 普攻 造成 %d 点伤害", r.Name, r.finalDamage(r.Rival.Attributes().trueDamage(r.Attack)))
-	r.Rival.DirectTakeDamage(round, r.finalDamage(r.Rival.Attributes().trueDamage(r.Attack)), 1, Normal)
+	trueDam := r.finalDamage(r.Rival.Attributes().trueDamage(r.Attack))
+	log.Print("%s 普攻 造成 %d 点伤害", r.Name, trueDam)
+	r.Rival.DirectTakeDamage(round, trueDam, 1, Normal)
 }
 
 func (r *TheRaven) finalDamage(damage int16) int16 {
