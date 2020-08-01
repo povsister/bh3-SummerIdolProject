44a5de67a1a1552667bfd3e4e870d30baeb1012d
povsister <pov@mahou-shoujo.moe>
2020-08-01 21:39:56 +0800

    Fix skill of Mei

--- player/Mei.go
+++ player/Mei.go
@@ -16,14 +16,15 @@ func (m *RaidenMei) RoundAttack(round uint16) {
 	if m.tryRecover() {
 		return
 	}
-	defer m.tryParalyze(round)
 	if round%2 == 0 {
 		log.Print("%s 发动技能 雷电家的龙女仆! 造成 5 x 3 点元素伤害", m.Name)
 		m.Rival.DirectTakeDamage(round, 3, 5, Unique)
+		m.tryParalyze(round)
 		return
 	}
 	log.Print("%s 普攻 造成 %d 点伤害", m.Name, m.Rival.Attributes().trueDamage(m.Attack))
 	m.Rival.TakeDamage(round, m.Attack, 1, Normal)
+	m.tryParalyze(round)
 }
 
 func (m *RaidenMei) tryParalyze(round uint16) {
