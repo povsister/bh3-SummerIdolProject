de55f12bb5dd2ea802c653a5433d16ca3ea33037
povsister <pov@mahou-shoujo.moe>
2020-08-02 19:46:51 +0800

    Fix skill of Kiana

--- player/Kiana.go
+++ player/Kiana.go
@@ -18,7 +18,7 @@ func (k *KianaKaslana) RoundAttack(round uint16) {
 	}
 	if round%2 == 0 && k.Rival.CanIUseSkill(round, "吃我一矛!") {
 		log.Print("%s 发动技能 吃我一矛! 造成 %d 点伤害", k.Name, k.Rival.Attributes().trueDamage(k.Attack+2*k.Rival.Attributes().Defence))
-		k.Rival.TakeDamage(round, k.Attack+2*k.Rival.Attributes().Defence, 1, Skill)
+		k.Rival.TakeDamage(round, k.Attack+2*k.Rival.Attributes().Defence, 1, Unique)
 		if k.Rand(35) {
 			log.Print("%s 因为 音浪~太强~ 眩晕一回合", k.Name)
 			k.stunned = true
